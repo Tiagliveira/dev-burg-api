23efc65a1e4cab764213d340b869cb1d1e718292
Tiagliveira <tiagliveira92@gmail.com>
2025-10-18 01:10:28 -0300

    foi feita alteração no controlles para incluir as relações feita no banco

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import Category from '../models/Category.js';
 import Product from './../models/Product.js';
 
 class ProductController {
@@ -6,7 +7,7 @@ class ProductController {
     const schema = Yup.object({
       name: Yup.string().required(),
       price: Yup.number().required(),
-      category: Yup.string().required(),
+      category_id: Yup.number().required(),
     });
 
     try {
@@ -16,20 +17,26 @@ class ProductController {
       return response.status(400).json({ error: err.errors });
     }
 
-    const { name, price, category } = request.body;
+    const { name, price, category_id } = request.body;
     const { filename } = request.file;
 
     const newProduct = await Product.create({
       name,
       price,
-      category,
+      category_id,
       path: filename,
     });
 
     return response.status(201).json({ newProduct });
   }
   async index(_request, response) {
-    const products = await Product.findAll();
+    const products = await Product.findAll({
+      include: {
+        model: Category,
+        as: 'category',
+        attributes: ['id', 'name'],
+      },
+    });
 
     console.log(_request.userId);
 
